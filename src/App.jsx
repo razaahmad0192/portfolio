a1874b37d9b1cf65886e90b79c284cf40418c95e
razaahmad0192 <razaahmad0192@gmail.com>
2025-10-11 15:47:28 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import LazyComponent from './components/LazyComponent'
 import ScrollComponent from './components/ScrollComponent'
 const App = () => {
   return (
-    <div className='text-white '>
+    <div className='text-black dark:text-white bg-white dark:bg-black '>
       <ScrollComponent/>
 
       <Navbar />
