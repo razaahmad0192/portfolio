ced5b4af50e4edcea88eea2729de7df9ee3aa1e5
razaahmad0192 <razaahmad0192@gmail.com>
2025-10-15 22:42:00 -0700

    Update HamburgerComponent.jsx

--- src/components/HamburgerComponent.jsx
+++ src/components/HamburgerComponent.jsx
@@ -1,7 +1,6 @@
 'use client';
 import { useState,useEffect } from 'react'
 import Hamburger from 'hamburger-react'
-import { FaMoon } from "react-icons/fa";
 import { Link } from "react-scroll";
 
 
